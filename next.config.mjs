d51629a0e60309f1074bf7325d5c51b801cf8acc
Evgeny Borodin <54502039+bor1eu@users.noreply.github.com>
2025-06-02 13:23:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    /**
@@ -5,17 +7,23 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  output: "export",
+  // output: "export",
 
-   reactStrictMode: true,
+   // reactStrictMode: true,
 
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/recipe-app",
+  // basePath: "/recipe-app",
 
+   basePath: isProd ? '/recipe-app' : '',
+   assetPrefix: isProd ? '/recipe-app/' : '', 
+   images: { unoptimized: true, 
+            // GitHub Pages does not support Next.js image optimization 
+   },
+            
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
